1af011dabeb5af053f5517db0fa2848a35211a46
Ryan Hyma <ryan.hyma@gmail.com>
2020-08-18 15:46:59 -0400

    Add decrypt.sh to scripts to github action workflow

---
+++ scripts/decrypt.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+export GPG_TTY=$(tty)
+ls -la
+gpg --quiet --batch --yes --decrypt --passphrase="$CONFIG_INI_PASSPHRASE" \
+--output ../src/main/java/resources/application.properties ../src/main/java/resources/application.properties.gpg
+ls -la
