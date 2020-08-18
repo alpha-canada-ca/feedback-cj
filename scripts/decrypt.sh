75fb74178be2c1fbd3f99a8f2594e47578f97443
Ryan Hyma <ryan.hyma@gmail.com>
2020-08-18 15:54:34 -0400

    Encrypted properties file for deployment

--- scripts/decrypt.sh
+++ scripts/decrypt.sh
@@ -2,5 +2,5 @@
 export GPG_TTY=$(tty)
 ls -la
 gpg --quiet --batch --yes --decrypt --passphrase="$CONFIG_INI_PASSPHRASE" \
---output ../src/main/java/resources/application.properties ../src/main/java/resources/application.properties.gpg
+--output ./src/main/java/resources/application.properties ./src/main/java/resources/application.properties.gpg
 ls -la
