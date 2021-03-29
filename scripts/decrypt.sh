ef5034c069abfb75394b3d68d7275c11f3ea10dc
Hamza Aburaneh <Hamza.Aburaneh@carleton.ca>
2021-03-29 15:10:27 -0400

    Review for containerizing script

--- scripts/decrypt.sh
+++ scripts/decrypt.sh
@@ -4,3 +4,9 @@ ls ./src/main/resources
 gpg --quiet --batch --yes --decrypt --passphrase="$APPLICATION_PROPERTIES_PASSPHRASE" \
 --output ./src/main/resources/application.properties ./src/main/resources/application.properties.gpg
 ls ./src/main/resources
+
+mkdir config
+ls -la
+gpg --quiet --batch --yes --decrypt --passphrase="$APPLICATION_PROPERTIES_PASSPHRASE" \
+--output ./config/config.ini config.ini.gpg
+ls -la ./config
