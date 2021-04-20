eae0b1dd9192702da634584e62babac033cdcb0f
Hamza Aburaneh <Hamza.Aburaneh@carleton.ca>
2021-04-20 10:14:33 -0400

    Update csv-archive.sh

--- csv-archive/csv-archive.sh
+++ csv-archive/csv-archive.sh
@@ -1,9 +1,9 @@
 #!/bin/s
 ls
 older_Than_x_Days=$(date --date="55 days ago" +%F)
-mongoexport --uri=$mongo_db_uri --collection=problem -q='{ "problemDate": { "$lte" : "'"$older_Than_x_Days"'"}}' --type=csv --out=text2.csv --fields=problemDate,problemDetails,problem,tags,url,language,institution,section,theme
+mongoexport --uri='$mongo_db_uri' --collection=problem -q='{ "problemDate": { "$lte" : "'"$older_Than_x_Days"'"}}' --type=csv --out=text2.csv --fields=problemDate,problemDetails,problem,tags,url,language,institution,section,theme
 ls
-sed -i '' \
+sed -i 'text2.csv' \
   -Ee 's/[][]//g' \
   -e "1s/problemDate/Date/g" \
   -e "1s/problemDetails/Comment/g" \
