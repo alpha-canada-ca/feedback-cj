e9fa7bbf6f7e3b94f22a499e7660af7707236809
Hamza Aburaneh <Hamza.Aburaneh@carleton.ca>
2021-04-27 13:01:11 -0400

    Decrypt spreadsheet credentials

--- scripts/decrypt.sh
+++ scripts/decrypt.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 export GPG_TTY=$(tty)
 ls ./src/main/resources
+
 gpg --quiet --batch --yes --decrypt --passphrase="$APPLICATION_PROPERTIES_PASSPHRASE" \
 --output ./src/main/resources/application.properties ./src/main/resources/application.properties.gpg
+
+gpg --quiet --batch --yes --decrypt --passphrase="$APPLICATION_PROPERTIES_PASSPHRASE" \
+--output ./src/main/resources/spreadsheetCredentials.json ./src/main/resources/spreadsheetCredentials.json.gpg
+
+gpg --quiet --batch --yes --decrypt --passphrase="$APPLICATION_PROPERTIES_PASSPHRASE" \
+--output ./tokens/StoredCredential ./tokens/StoredCredential.gpg
+
 ls ./src/main/resources
