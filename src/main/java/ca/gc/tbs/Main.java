958dae3817cfa78a47812cad52b5154857796f4d
Hamza Aburaneh <Hamza.Aburaneh@carleton.ca>
2021-07-05 14:05:03 -0400

    Update Main.java

--- src/main/java/ca/gc/tbs/Main.java
+++ src/main/java/ca/gc/tbs/Main.java
@@ -160,10 +160,11 @@ public class Main implements CommandLineRunner {
 	public Main() throws Exception {
   
 	}
-	
+	private ContentService contentService2 = new ContentService();
 	// Main Loop, Runs all functions needed.
 	@Override
 	public void run(String... args) throws Exception {
+		
 		this.AirTableKey = new Airtable().configure(this.airtableKey);
 		
 		this.mainBase = this.AirTableKey.base(this.problemAirtableBase);
