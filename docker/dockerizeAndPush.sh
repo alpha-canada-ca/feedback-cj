4c63851dda07267d520db95a1ad46da8db9a9a75
Hamza Aburaneh <Hamza.Aburaneh@carleton.ca>
2022-10-05 10:06:24 -0400

    change directory

--- docker/dockerizeAndPush.sh
+++ docker/dockerizeAndPush.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd ..
 mvn package
-docker build . -f ./scripts/Dockerfile -t tbsacr.azurecr.io/pagefeedback-cj:1.0.0 
+docker build . -f ./docker/Dockerfile -t tbsacr.azurecr.io/pagefeedback-cj:1.0.0
 docker push tbsacr.azurecr.io/pagefeedback-cj:1.0.0
