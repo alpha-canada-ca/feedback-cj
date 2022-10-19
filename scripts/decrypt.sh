d3f11d8b2b8969fcae62d4639d45d885325b7f47
Hamza Aburaneh <Hamza.Aburaneh@carleton.ca>
2022-10-19 09:20:49 -0400

    rename file

--- scripts/decrypt.sh
+++ scripts/decrypt.sh
@@ -5,6 +5,6 @@ ls ./src/main/resources
 
 gpg --quiet --batch --yes  --passphrase="$APPLICATION_PROPERTIES_PASSPHRASE" --output ./src/main/resources/application.properties --decrypt ./src/main/resources/application.properties.gpg
 
-gpg --quiet --batch --yes  --passphrase="$APPLICATION_PROPERTIES_PASSPHRASE" --output ./src/main/resources/service_account.p12 --decrypt ./src/main/resources/service_account.p12.gpg
+gpg --quiet --batch --yes  --passphrase="$APPLICATION_PROPERTIES_PASSPHRASE" --output ./src/main/resources/service-account.p12 --decrypt ./src/main/resources/service-account.p12.gpg
 
 ls ./src/main/resources
